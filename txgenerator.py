da76ad3302301e1e671a3232d573e73046dcf545
Mizeva G <wolf.triumph@gmail.com>
2019-05-07 06:13:18 +1000

    Initial a project

---
+++ txgenerator.py
@@ -0,0 +1,19 @@
+import pyforms
+from   pyforms.basewidget import BaseWidget
+from   pyforms.controls import ControlText
+from   pyforms.controls import ControlButton
+
+class TxGenerator(BaseWidget):
+
+    def __init__(self):
+        super(TxGenerator,self).__init__('TxGenerator 1.0')
+
+        #Definition of the forms fields
+        self._firstname  = ControlText('First name', 'Default value')
+        self._middlename = ControlText('Middle name')
+        self._lastname   = ControlText('Lastname name')
+        self._fullname   = ControlText('Full name')
+        self._button     = ControlButton('Press this button')
+
+#Execute the application
+if __name__ == "__main__": pyforms.start_app( TxGenerator )
