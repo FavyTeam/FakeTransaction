c4a31626628c686d455db0f635f326a74834e6bd
Mizeva G <wolf.triumph@gmail.com>
2019-05-07 06:56:37 +1000

    update setting.py as settings.py

--- txgenerator.py
+++ txgenerator.py
@@ -12,13 +12,16 @@ class TxGenerator(BaseWidget):
 
     def __init__(self):
         super(TxGenerator,self).__init__('TxGenerator 1.0')
-
+        self.setupUI()
+    
+    def setupUI(self) :
         #Definition of the forms fields
         self._firstname  = ControlText('First name', 'Default value')
         self._middlename = ControlText('Middle name')
         self._lastname   = ControlText('Lastname name')
         self._fullname   = ControlText('Full name')
         self._button     = ControlButton('Press this button')
+        
 
 #Execute the application
 if __name__ == "__main__": pyforms.start_app( TxGenerator )
